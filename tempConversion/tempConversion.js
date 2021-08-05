ea41c19685dca1ef9677c1000dd9b1b4f6309338
João Henrique Santana <joaoenrique.san@gmail.com>
2021-08-05 19:23:27 -0300

    Solve tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let result = (tempF - 32) * 5/9;
+  result = Math.round(result * 10) / 10; //Rounds to one decimal 
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let result = tempC * 9/5 + 32;
+  result = Math.round(result * 10) / 10; //Rounds to one decimal 
+  return result;
 };
 
 module.exports = {
