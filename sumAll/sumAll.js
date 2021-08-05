2599f62e18ad058b7c430b44dfd9b30552cf4027
João Henrique Santana <joaoenrique.san@gmail.com>
2021-08-05 18:47:27 -0300

    Solve sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, lastNumber) {
+    let actualNumber = firstNumber;
+    let sum = 0;
+    console.log(typeof firstNumber);
+    if(typeof firstNumber != "number"|| typeof lastNumber != "number" || firstNumber < 0 || lastNumber < 0){
+        return 'ERROR';
+    }
+    while(actualNumber != lastNumber){
+        sum += actualNumber;
+        actualNumber < lastNumber ? actualNumber++ : actualNumber--; 
+    }
+    sum += lastNumber;
+    
+    return sum;
 };
 
 module.exports = sumAll;
