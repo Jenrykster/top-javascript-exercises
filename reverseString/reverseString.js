150161dd6a7cc415b74d794fa4596a39574b9c0e
João Henrique Santana <joaoenrique.san@gmail.com>
2021-08-05 15:33:36 -0300

    Solve reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let finalString = '';
+    for(let i = stringToReverse.length - 1; i >= 0; i--){
+        finalString+=stringToReverse[i];
+    }
+    return finalString;
 };
 
 module.exports = reverseString;
