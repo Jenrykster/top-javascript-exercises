5005da05346f53e172ad47bb5d2cfb90446f6d0a
João Henrique Santana <joaoenrique.san@gmail.com>
2021-08-05 14:12:26 -0300

    Solve repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(whatToRepeat, nOfTimes) {
+    let finalString = '';
+    if(nOfTimes < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < nOfTimes; i++){
+        finalString+= whatToRepeat;
+    }
+    return finalString;
 };
 
 module.exports = repeatString;
