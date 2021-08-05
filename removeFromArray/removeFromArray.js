b4c7717fd45055075f143599475e7075ea333c07
João Henrique Santana <joaoenrique.san@gmail.com>
2021-08-05 16:17:30 -0300

    Solve removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(array, ...itemsToRemove) {
+    arrayToReturn = array;
+    for(let i = 0; i <= itemsToRemove.length - 1; i++ ){
+        let index = array.indexOf(itemsToRemove[i]);
+        if(index > -1){
+            arrayToReturn.splice(index, 1);
+        }
+    }
+    return arrayToReturn;
+}
 module.exports = removeFromArray;
