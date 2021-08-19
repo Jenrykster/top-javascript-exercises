ef0bc94320d31eb064055262a500d5959f025c9a
João Henrique Santana <joaoenrique.san@gmail.com>
2021-08-18 23:48:31 -0300

    Solve palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let words = text.toLowerCase().split(' ').join('');
+    words = words.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ");
+    let reversedWords = words.split('').reverse().join('');
+    console.log(words);
+    return words == reversedWords;
 };
 
 module.exports = palindromes;
