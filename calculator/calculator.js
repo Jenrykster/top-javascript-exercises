515b643288caa884ce7fa28fab9e2d8d0c80a457
João Henrique Santana <joaoenrique.san@gmail.com>
2021-08-18 23:23:36 -0300

    Solve calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if(array.length == 0) return 0;
+  return array.reduce((a,b)=> a+b);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if(array.length == 0) return 0;
+  return array.reduce((a,b)=> a*b);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let total = a;
+	for(let i = 0; i < b-1; i++){
+    total *= a
+  }
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let total = 1;
+  for(let i = 1; i <= number; i++){
+    total *= i;
+  }
+  return total;
 };
 
 module.exports = {
