dc11d1a56a03a132ec38a553eb0d66c94ddd263c
João Henrique Santana <joaoenrique.san@gmail.com>
2021-08-23 00:37:36 -0300

    Solve findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people.reduce((lastPerson, actual)=>{
+        let lastPersonAge;
+        let actPersonAge;
+        actPersonAge = actual.yearOfDeath - actual.yearOfBirth;
+        if(typeof lastPerson.yearOfDeath == 'undefined'){
+            let thisYear = new Date().getFullYear();
+            lastPersonAge = thisYear - lastPerson.yearOfBirth;
+        }else{
+            lastPersonAge = lastPerson.yearOfDeath - lastPerson.yearOfBirth;
+        }
+        console.log(actPersonAge, lastPersonAge)
+        return actPersonAge > lastPersonAge ? actual : lastPerson;
+        
+    })
+    return oldest;
 };
 
 module.exports = findTheOldest;
