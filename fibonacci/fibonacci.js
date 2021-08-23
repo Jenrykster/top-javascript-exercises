e080e79180a6d460d8697076c38b9fc9db33f858
João Henrique Santana <joaoenrique.san@gmail.com>
2021-08-22 23:54:05 -0300

    Solve fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let fib = 0;
+    let lastNumber = 1;
+    let nextNumber = 1;
+    if(number < 0) return "OOPS";
+    if(number < 3) return 1;
+    for(let i = 0; i < number - 2; i++){
+        fib = lastNumber + nextNumber;
+        lastNumber = nextNumber;
+        nextNumber = fib;
+        console.log(fib, lastNumber, nextNumber);
+    }
+    return fib;
 };
 
 module.exports = fibonacci;
